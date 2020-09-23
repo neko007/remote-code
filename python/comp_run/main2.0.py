4b3705dc0afeea29c1d616c2e4cc89b15d4a8f9b
zzz <zhaozhizhan@126.com>
2020-09-23 13:22:37 +0800

    将修改wps和wrf的函数拆分为两个

--- python/comp_run/main2.0.py
+++ python/comp_run/main2.0.py
@@ -1,47 +1,68 @@
+'''
+需要准备好WPS和WRF均编译完成的原始文件
+'''
+
 import os
 import subprocess as sp
 import numpy as np
-from revise_nml2 import *
+from modify_nml2 import *
 
 # 打印基本信息
 def describe(test_number):
     print(f'**** {test_number} start ****')
 
 # 编译运行wps
-def cc_wps(sst_update):
-    print('>>>> configure wps <<<<')
-    sp.call('echo 17 | ./configure > log.configure', shell=True)
-    print('>>>> compile wps <<<<')
-    sp.run('./compile >& log.compile', shell=True)
+def run_wps(sst_flag):
+    # 配置编译
+    # print('>>>> configure wps <<<<')
+    # sp.call('echo 17 | ./configure > log.configure', shell=True)
+    # print('>>>> compile wps <<<<')
+    # sp.run('./compile >& log.compile', shell=True)
+    
+    # 修改namelist.wps
+    nml_wps = modify_wps_nml(0)
+    # 复写 namelist.wps
+    rewrite_namelist(wps_dir, wps_nml_name, nml_wps, 1)
+    # geogrid 地形插值
     print('>>>> geogrid.exe <<<<')
     sp.run('./geogrid.exe >& log.geogrid', shell=True)
     # 链接数据
     print('>>>> link data <<<<')
     sp.run('ln -sf ungrib/Variable_Tables/Vtable.GFS Vtable', shell=True)
     sp.run('./link_grib.csh ' + os.path.join(data_dir, data_file), shell=True)
-     # ungrid 解码
+    # ungrid 解码
     print('>>>> ungrib.exe <<<<')
-    sp.run('./ungrib.exe >& log.ungrid', shell=True)
-    if sst_update == 1:
+    sp.run('./ungrib.exe', shell=True)
+    if sst_flag == 1:
+        # 链接海温场
         print('>>>> prepare SST data <<<<')
         sp.run('ln -sf ungrib/Variable_Tables/Vtable.SST Vtable', shell=True)
         sp.run('./link_grib.csh ' + os.path.join(data_dir, sst_file), shell=True)
         # 修改namelist.wps
-        nml_wps, nml_wrf = revise_nml(sst_update=1)
+        nml_wps = modify_nml_wps(1)
+        # 复写 namelist.wps
         rewrite_namelist(wps_dir, wps_nml_name, nml_wps, 1)
+        # ungrid 解码
         print('>>>> ungrib.exe <<<<')
-        sp.run('./ungrib.exe >& log.ungrid', shell=True)
-    # 将气象场插值到地形上
+        sp.run('./ungrib.exe', shell=True)
+    # metgrid 气象场插值
     print('>>>> metgrid.exe <<<<')
-    sp.run('./metgrid.exe >& log.metgrid', shell=True)
+    sp.run('./metgrid.exe', shell=True)
 
 
 # 运行WRF
-def cc_wrf():
+def run_wrf(sst_flag):
+    # 修改namelist.input
+    nml_wrf = modify_wrf_nml(sst_flag)
+    # 复写namelist.input
+    rewrite_namelist(os.path.join(wrf_dir, 'run'), wrf_nml_name, nml_wrf, 2)
+    # 链接气象场
     print('>>>> link met_em* <<<<')
     sp.run('ln -sf ../../WPS/met_em* .', shell=True)
+    # 生成初始场
     print('>>>> real.exe <<<<')
     sp.run('mpirun -np 1 ./real.exe', shell=True)
+    # 运行wrf
     print('>>>> wrf.exe <<<<')
     sp.run(f'mpirun -np {core_num} ./wrf.exe', shell=True)
 
@@ -57,19 +78,11 @@ def rewrite_namelist(target_dir, nml_name, nml, flag):
     nml.write(nml_name)
 
 if __name__ == '__main__':
-    # 修改namelist
-    nml_wps, nml_wrf, sst_update = revise_nml(sst_update=0)
-
+    # 打印基本信息
     describe(test_number)
 
-    # 重新写namelist.wps
-    rewrite_namelist(wps_dir, wps_nml_name, nml_wps, 1)
-
-    # 配置编译运行WPS
-    cc_wps(sst_update)
-
-    # 重新写namlist.input
-    rewrite_namelist(os.path.join(wrf_dir, 'run'), wrf_nml_name, nml_wrf, 2)
+    # 运行WPS
+    run_wps(sst_flag)
 
     # 运行WRF
-    cc_wrf()
+    run_wrf(sst_flag)
---
+++ python/comp_run/modify_nml2.py
@@ -0,0 +1,209 @@
+import os
+import f90nml 
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# 需要手动修改的部分
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# 运行设置
+core_num = 16
+
+# 基本路径
+model_dir = '/home/zzhzhao/Model'
+test_number = ''
+
+# 驱动场资料路径
+data_file = 'GFS_*'
+sst_file = ' '
+
+# 驱动场资料时间间隔
+interval_hours = 6
+interval_seconds = interval_hours * 3600 
+
+# 驱动场资料层数
+num_metgrid_levels = 32
+
+# 模拟的起止时间
+y_start, m_start, d_start, h_start = 2020, 9, 1, 00
+y_end, m_end, d_end, h_end = 2020, 9, 3, 00
+run_days = 2
+run_hours = 0
+
+# restart设置
+restart = False
+restart_interval = 1440 # minute
+
+# 文件输出时间间隔(min)、文件打包个数
+history_interval = [120, 120, 60]
+frames_per_outfile = [3, 3, 100]
+
+# 嵌套区域相关配置
+max_dom = 1
+parent_ids = [1, 1]
+parent_grid_ratios = [1, 3]
+
+# 每个区域的起止经纬度， 按照d01、d02...的顺序排列
+lat_mins = [
+    20, 
+    30
+    ]
+lat_maxs = [
+    55, 
+    45
+    ]
+lon_mins = [
+    80, 
+    105
+    ]
+lon_maxs = [
+    145, 
+    125
+    ]
+
+# 父区域分辨率
+dx = 30000
+dy = 30000
+
+# 积分步长
+time_step = dx / 1000 * 4
+
+# 引入海温场
+sst_file = 1
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# 不经常修改的部分
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# 路径
+wps_nml_name = 'namelist.wps'
+wrf_nml_name = 'namelist.input'
+tests_dir = os.path.join(model_dir, 'tests')
+wps_dir = os.path.join(tests_dir, test_number, 'WPS')
+wrf_dir = os.path.join(tests_dir, test_number, 'WRF')
+data_dir = '../data' # 驱动场资料路径
+geog_data_path = os.path.join(model_dir, 'Build_WRF/WPS_GEOG') # 静态地形资料的路径
+
+# 投影
+map_proj = 'lambert'
+truelat1 = 30. 
+truelat2 = 60. 
+
+# ungrib
+prefix = 'FILE'
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# 自动计算部分
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# 计算起止时间
+start_date = '%d-%02d-%02d_%02d:00:00' % (y_start, m_start, d_start, h_start)
+end_date = '%d-%02d-%02d_%02d:00:00' % (y_end, m_end, d_end, h_end)
+
+# 计算网格配置
+dxs = [] 
+dys = []
+e_wes = []
+e_sns = []
+i_parent_starts = []
+j_parent_starts = []
+for i in range(max_dom):
+    scale_cof = parent_grid_ratios[i] * parent_grid_ratios[parent_ids[i]-1]  
+    e_we = int((lon_maxs[i] - lon_mins[i]) * 110 * scale_cof / (dx * 1e-3))
+    e_sn = int((lat_maxs[i] - lat_mins[i]) * 110 * scale_cof / (dx * 1e-3))
+    if i+1 == 1:
+        i_parent_start = 1 
+        j_parent_start = 1
+    else:
+        k = parent_ids[i] - 1
+        scale_cof_parent = parent_grid_ratios[k] * parent_grid_ratios[parent_ids[k]-1]
+        i_parent_start = int((lon_mins[i] - lon_mins[k]) * 110 * scale_cof_parent / (dx * 1e-3))
+        j_parent_start = int((lat_mins[i] - lat_mins[k]) * 110 * scale_cof_parent / (dx * 1e-3))
+        while(e_we%3 != 1):
+            e_we += 1
+        while(e_sn%3 != 1):
+            e_sn += 1
+    dxs.append(int(dx/scale_cof))
+    dys.append(int(dy/scale_cof))
+    e_wes.append(e_we)
+    e_sns.append(e_sn)
+    i_parent_starts.append(i_parent_start)
+    j_parent_starts.append(j_parent_start)
+
+ref_lat = (lat_maxs[0] + lat_mins[0]) / 2.
+ref_lon = (lon_maxs[0] + lon_mins[0]) / 2. 
+stand_lon = ref_lon
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+def modify_wps_nml(sst_update):
+    '''
+    读取并修改namelist.wps
+    '''
+    nml_wps = f90nml.read(os.path.join(wps_dir, wps_nml_name))
+    nml_wps['share']['max_dom'] = max_dom
+    nml_wps['share']['start_date'] = [start_date for i in range(max_dom)]
+    nml_wps['share']['end_date'] = [end_date for i in range(max_dom)] 
+    nml_wps['share']['interval_seconds'] = interval_seconds
+
+    nml_wps['geogrid']['parent_id'] = parent_ids
+    nml_wps['geogrid']['parent_grid_ratio'] = parent_grid_ratios
+    nml_wps['geogrid']['i_parent_start'] = i_parent_starts
+    nml_wps['geogrid']['j_parent_start'] = j_parent_starts
+    nml_wps['geogrid']['e_we'] = e_wes 
+    nml_wps['geogrid']['e_sn'] = e_sns 
+    nml_wps['geogrid']['dx'] = dx
+    nml_wps['geogrid']['dy'] = dy 
+    nml_wps['geogrid']['map_proj'] = map_proj
+    nml_wps['geogrid']['ref_lat'] = ref_lat 
+    nml_wps['geogrid']['ref_lon'] = ref_lon
+    nml_wps['geogrid']['truelat1'] = truelat1
+    nml_wps['geogrid']['truelat2'] = truelat2
+    nml_wps['geogrid']['stand_lon'] = stand_lon 
+    nml_wps['geogrid']['geog_data_path'] = geog_data_path
+
+    nml_wps['ungrib']['prefix'] = prefix
+    if sst_update == 1:
+        nml_wps['ungrib']['prefix'] = 'SST'
+        nml_wps['metgrid']['fg_name'] = ['FILE', 'SST']
+    
+    return nml_wps
+    
+def modify_wrf_nml(sst_update):
+    '''
+    读取并修改namelist.input 
+    '''
+    nml_wrf = f90nml.read(os.path.join(wrf_dir, 'run', wrf_nml_name))
+    nml_wrf['time_control']['run_days'] = run_days
+    nml_wrf['time_control']['run_hours'] = run_hours
+    nml_wrf['time_control']['start_year'] = [y_start for i in range(max_dom)]
+    nml_wrf['time_control']['start_month'] = [m_start for i in range(max_dom)]
+    nml_wrf['time_control']['start_day'] = [d_start for i in range(max_dom)]
+    nml_wrf['time_control']['start_hour'] = [h_start for i in range(max_dom)]
+    nml_wrf['time_control']['end_year'] = [y_end for i in range(max_dom)]
+    nml_wrf['time_control']['end_month'] = [m_end for i in range(max_dom)]
+    nml_wrf['time_control']['end_day'] = [d_end for i in range(max_dom)]
+    nml_wrf['time_control']['end_hour'] = [h_end for i in range(max_dom)]
+    nml_wrf['time_control']['interval_seconds'] = interval_seconds
+    nml_wrf['time_control']['history_interval'] = history_interval
+    nml_wrf['time_control']['frames_per_outfile'] = frames_per_outfile
+    nml_wrf['time_control']['restart'] = restart
+    nml_wrf['time_control']['restart_interval'] = restart_interval
+
+    nml_wrf['domains']['max_dom'] = max_dom
+    nml_wrf['domains']['time_step'] = time_step
+    nml_wrf['domains']['e_we'] = e_wes
+    nml_wrf['domains']['e_sn'] = e_sns
+    nml_wrf['domains']['num_metgrid_levels'] = num_metgrid_levels
+    nml_wrf['domains']['dx'] = dxs 
+    nml_wrf['domains']['dy'] = dys
+    nml_wrf['domains']['parent_id'] = [0, 1, 2] # warning
+    nml_wrf['domains']['i_parent_start'] = i_parent_starts
+    nml_wrf['domains']['j_parent_start'] = j_parent_starts
+    nml_wrf['domains']['parent_grid_ratio'] = parent_grid_ratios
+    nml_wrf['domains']['parent_time_step_ratio'] = parent_grid_ratios
+    nml_wrf['domains'].update({'sfcp_to_sfcp':True})
+    if sst_update == 1:
+        nml_wrf['time_control'].update({'auxinput4_inname':'wrflowinp_d<domain>', 'auxinput4_interval':360, 'io_form_auxinput4':2})
+        nml_wrf['physics'].update({'sst_update':sst_update})
+
+    return nml_wrf
